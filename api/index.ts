4716641580a2962d621aea7124e2fe59b83d65b3
BigManRoss <bigmanrosssteam@gmail.com>
2024-05-27 00:34:24 +0200

    test: testing vercel

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const startBot = require("./startBot.js");
+const startBot = require("../startBot.js");
 
 app.get("/", (req, res) => {
     startBot();
